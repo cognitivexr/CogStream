1b1e5b7e309070d63d6770809124301c2a37a279
Thomas Rausch <thomas@thrau.at>
2021-03-14 15:53:09 +0100

    Add engines API

---
+++ mediator/pkg/runtime/engines.go
@@ -0,0 +1,17 @@
+package runtime
+
+import (
+	"cognitivexr.at/cogstream/pkg/api/engines"
+)
+
+type EngineFinder interface {
+	ListEngines() []*engines.Engine
+	FindEngines(engines.Specification) []*engines.Engine
+	FindEngineByName(string) (*engines.Engine, bool)
+}
+
+type EngineRuntime interface {
+	StartEngine(engine *engines.Engine) (*engines.RunningEngine, error)
+	StopEngine(*engines.RunningEngine) error
+	ListRunning() *engines.RunningEngine
+}
