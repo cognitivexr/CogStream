fc56df0b6f5450375c086b178e6a9285e32a4c88
Thomas Rausch <thomas@rauschig.org>
2021-04-10 19:03:52 +0200

    Add more io tests for cogstream-py

--- cogstream-py/tests/test_io.py
+++ cogstream-py/tests/test_io.py
@@ -1,7 +1,10 @@
 import time
 from unittest import TestCase
 
-from cogstream.engine.io import SocketResultWriter, SocketResultScanner, ResultPacket
+import numpy as np
+
+from cogstream.engine.io import SocketResultWriter, SocketResultScanner, ResultPacket, SocketFrameWriter, \
+    SocketFrameScanner, FramePacket
 
 
 class FakeSocketPipe:
@@ -55,3 +58,48 @@ class TestSocketResultWriterScanner(TestCase):
         a1 = scanner.next()
 
         self.assertEqual(e1, a1)
+
+
+class TestSocketFrameWriterScanner(TestCase):
+    def test_write_scan(self):
+        pipe = FakeSocketPipe()
+
+        writer = SocketFrameWriter(pipe)
+        scanner = SocketFrameScanner(pipe)
+
+        e1 = FramePacket(1, 42, time.time(), b'metadata', np.asarray([1, 2, 3]).tobytes())
+        e2 = FramePacket(1, 43, time.time(), b'metadata', np.asarray([2, 4, 6]).tobytes())
+
+        writer.write(e1)
+        writer.write(e2)
+
+        a1 = scanner.next()
+        a2 = scanner.next()
+
+        self.assertEqual(e1, a1)
+        self.assertEqual(e2, a2)
+
+    def test_write_scan_empty_metadata(self):
+        pipe = FakeSocketPipe()
+
+        writer = SocketFrameWriter(pipe)
+        scanner = SocketFrameScanner(pipe)
+
+        e1 = FramePacket(1, 42, time.time(), None, np.asarray([1, 2, 3]).tobytes())
+        e2 = FramePacket(1, 43, time.time(), b'', np.asarray([1, 2, 3]).tobytes())
+
+        writer.write(e1)
+        writer.write(e2)
+
+        a1 = scanner.next()
+        a2 = scanner.next()
+
+        self.assertEqual(e1, a1)
+        self.assertEqual(FramePacket(e2.stream_id, e2.frame_id, e2.timestamp, None, e2.data), a2)
+
+    def test_scan_empty_returns_None(self):
+        pipe = FakeSocketPipe()
+
+        scanner = SocketFrameScanner(pipe)
+
+        self.assertIsNone(scanner.next())
