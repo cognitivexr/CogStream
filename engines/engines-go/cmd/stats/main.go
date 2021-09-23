3c8f38ad047b39c5fdbde194021044c716934a71
Silvio Vasiljevic <silvio.vasiljevic@gmail.com>
2021-09-23 12:52:20 +0200

    Update usage of stats engine for non-webrtc

--- engines/engines-go/cmd/stats/main.go
+++ engines/engines-go/cmd/stats/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"cognitivexr.at/cogstream/engines/pkg/engines/stats"
-	"cognitivexr.at/cogstream/pkg/webrtc"
+	"cognitivexr.at/cogstream/pkg/serve"
 	"context"
 	"log"
 )
 
 func main() {
 	ctx := context.Background()
-	err := webrtc.ServeEngineNetwork(ctx, stats.Factory)
+	err := serve.ServeEngineNetwork(ctx, "tcp", "0.0.0.0:53210", stats.Factory)
 	log.Printf("engine server returned: %v", err)
 }
