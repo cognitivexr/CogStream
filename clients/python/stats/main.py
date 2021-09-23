0412a0f59a71db3868828c4bbef94dde6e88d6fc
Silvio Vasiljevic <silvio.vasiljevic@gmail.com>
2021-09-23 12:52:20 +0200

    Update usage of stats engine for non-webrtc

--- clients/python/stats/main.py
+++ clients/python/stats/main.py
@@ -10,7 +10,12 @@ from cogstream.typing import deep_from_dict
 
 
 def main():
-    stream_spec = deep_from_dict({"engineAddress": "127.0.0.1:53210", "attributes": {}}, StreamSpec)
+    stream_spec = StreamSpec('127.0.0.1:53210', to_attributes({
+        "format.width": 1920,
+        "format.height": 1080,
+        "format.orientation": 1,
+        "format.colorMode": 1
+    }))
     engine_client = EngineClient(stream_spec)
     engine_client.open()
 
@@ -19,12 +24,12 @@ def main():
         engine_client.close()
         return
 
-    cap = cv2.VideoCapture("/home/vader/Videos/sample_1920x1080.mp4")
+    cap = cv2.VideoCapture("/home/silvio/videos/sample/1920x1080.mp4")
+
+    if not cap.isOpened():
+        raise Exception("Error opening video stream or file")
 
     try:
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-
         stream_camera(cap, engine_client)
     except KeyboardInterrupt:
         pass
